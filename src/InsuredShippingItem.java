316d46e1a5ae309e09c33161bf6ea63a4bc48da2
student <evelyns60@nycstudents.net>
2023-03-20 10:59:06 -0400

    3/20/23 class push

---
+++ src/InsuredShippingItem.java
@@ -0,0 +1,18 @@
+public class InsuredShippingItem extends ShippingItem {
+  private double insuredAmount;
+  
+  public InsuredShippingItem(double w, double insAmt) {
+    super(w);
+    insuredAmount = insAmt;
+  }
+
+  @Override
+  public double getCost() {
+    return super.getCost() + insuredAmount;
+  }
+  
+  public void addMoreInsurance(double amt) {
+    insuredAmount += amt;
+  }
+
+}
